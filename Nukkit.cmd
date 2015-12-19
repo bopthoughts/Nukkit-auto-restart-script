a2f5fa9f1d8682a9fa58abbad865ca5ebeef4c2c
da123rrell <darrell.darel@gmail.com>
2015-12-19 09:43:25 +0700

    added script

---
+++ Nukkit.cmd
@@ -0,0 +1,43 @@
+@echo off
+TITLE Nukkit : Auto restart
+cd /d %~dp0
+netstat -o -n -a | findstr 0.0.0.0:19133>nul
+if %ERRORLEVEL% equ 0 (
+    echo Your server is running.
+    goto :loop
+) ELSE (
+    echo Starting your Nukkit server.
+    goto :StartNt
+)
+
+
+:loop
+echo Checking if server is online...
+PING 127.0.0.1 -n 10 >NUL
+
+
+netstat -o -n -a | findstr 0.0:11222>nul
+if %ERRORLEVEL% equ 0 (
+    echo Server is running.
+    goto :loop
+) ELSE (
+    echo Starting Nukkit in 3 seconds...
+    PING 127.0.0.1 -n 3 >NUL
+    goto :StartPM
+)
+
+
+:StartNt
+
+IF EXIST Nukkit.exe (
+	start cmd /k echo hello
+) ELSE (
+	IF EXIST Nukkit_64bit.exe (
+		start cmd /k @Nukkit_64bit.exe
+	) ELSE (
+		ECHO [FATAL] No nukkit.jar file found!
+		ECHO [INFO] Download the file at nukkit.ru
+	)
+)
+goto :loop
+PAUSE
